6f64dd714d38cbd43971866b99e406264288a23a
Reza <rdehghani.akorn@gmail.com>
2025-09-09 15:10:12 +0330

    models was refoctored

--- SaharBeautyWeb/Models/Commons/ViewComponentErrorDto.cs
+++ SaharBeautyWeb/Models/Commons/ViewComponentErrorDto.cs
@@ -3,7 +3,6 @@
 public class ViewComponentErrorDto
 {
     public bool IsSuccess { get; set; }
-    public string? Description { get; set; }
     public string? Error { get; set; }
     public int StatusCode { get; set; }
 }
