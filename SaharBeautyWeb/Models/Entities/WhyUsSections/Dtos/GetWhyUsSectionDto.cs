9f2a651d7d9d8e0c973e206105875f7210c8b14c
Reza <rdehghani.akorn@gmail.com>
2025-09-15 14:40:31 +0330

    refactor add why us

--- SaharBeautyWeb/Models/Entities/WhyUsSections/Dtos/GetWhyUsSectionDto.cs
+++ SaharBeautyWeb/Models/Entities/WhyUsSections/Dtos/GetWhyUsSectionDto.cs
@@ -5,8 +5,8 @@ namespace SaharBeautyWeb.Models.Entities.WhyUsSections.Dtos;
 public class GetWhyUsSectionDto
 {
     public long Id { get; set; }
-    public string Title { get; set; } = default!;
-    public string Description { get; set; } = default!;
+    public required string Title { get; set; } 
+    public required string Description { get; set; }
     public ImageDetailsDto? Image { get; set; }
     public List<GetWhyUsQuestionDto> Questions { get; set; } = new();
 }
