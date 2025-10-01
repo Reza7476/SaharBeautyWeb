9701e5422c531f8ce05e6a5c75b84cc013465ce0
Reza <rdehghani.akorn@gmail.com>
2025-10-01 16:38:13 +0330

    about us was gotten for display

--- SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/SiteSettings/AboutUs/AboutUs.js
+++ SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/SiteSettings/AboutUs/AboutUs.js
@@ -37,7 +37,22 @@
         });
     });
 
-    var map = L.map('map').setView([35.6892, 51.3890], 13); // مرکز تهران، می‌توانید تغییر دهید
+
+    if ($("#map").length) {
+        GetMap();
+    }
+    if ($("#display-salon-map").length) {
+        var lat = $("#salon-latitude").val();
+        var long = $("#salon-longitude").val()
+        ShowMap(lat, long);
+    }
+});
+
+
+
+function GetMap() {
+
+    var map = L.map('map').setView([29.6100, 52.5400], 13); // مرکز شیراز، می‌توانید تغییر دهید
 
     // اضافه کردن Tile Layer از OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -45,23 +60,40 @@
     }).addTo(map);
 
     var marker;
-
     map.on('click', function (e) {
         var lat = e.latlng.lat.toFixed(6);
         var lng = e.latlng.lng.toFixed(6);
-
         // اگر قبلا مارکر بود، حذف کن
         if (marker) {
             map.removeLayer(marker);
         }
-
         // اضافه کردن مارکر جدید
         marker = L.marker([lat, lng]).addTo(map);
-
         // مقدار hidden input ها را ست کن
         document.getElementById('latitude').value = lat;
         document.getElementById('longitude').value = lng;
     });
+}
+function ShowMap(lat, long) {
+    var salaonLat = lat || 29.6100;
+    var salonLng = long || 52.5400;
 
+    // ایجاد نقشه روی مختصات
+    var map12 = L.map('display-salon-map', {
+        dragging: false,   // غیر فعال کردن حرکت نقشه
+        touchZoom: true,
+        scrollWheelZoom: true,
+        doubleClickZoom: false,
+        boxZoom: false,
+        keyboard: false,
+        zoomControl:true
+    }).setView([salaonLat, salonLng], 15); // زوم نزدیک
 
-});
+    // اضافه کردن لایه Tile
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map12);
+
+    // اضافه کردن مارکر روی نقطه
+    L.marker([salaonLat, salonLng]).addTo(map12);
+}
