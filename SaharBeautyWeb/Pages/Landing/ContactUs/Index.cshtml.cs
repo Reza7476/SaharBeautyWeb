19869905890f29c8dc564675a0db0e15ceaa60d2
Reza <rdehghani.akorn@gmail.com>
2025-10-07 15:24:43 +0330

    base page model error for about us was handled

--- SaharBeautyWeb/Pages/Landing/ContactUs/Index.cshtml.cs
+++ SaharBeautyWeb/Pages/Landing/ContactUs/Index.cshtml.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using SaharBeautyWeb.Models.Entities.AboutUs.Management.Models;
+using SaharBeautyWeb.Pages.Shared;
 using SaharBeautyWeb.Services.AboutUs;
 
 namespace SaharBeautyWeb.Pages.Landing.ContactUs;
 
-public class IndexModel : PageModel
+public class IndexModel : LandingBasePageModel
 {
     private readonly IAboutUsService _aboutUsService;
 
@@ -16,12 +16,13 @@ public class IndexModel : PageModel
         _aboutUsService = aboutUsService;
     }
 
-    public async Task<IActionResult>  OnGet()
+    public async Task<IActionResult> OnGet()
     {
         var result = await _aboutUsService.GeAboutUs();
-        if(result.IsSuccess && result.Data!=null)
+        var response = HandleApiResult(result);
+        if (result != null && result.IsSuccess && result.Data != null)
         {
-            ModelData = new GetAboutUsModel()
+            ModelData = new GetAboutUsModel
             {
                 MobileNumber = result.Data.MobileNumber,
                 Telephone = result.Data.Telephone,
@@ -30,16 +31,12 @@ public class IndexModel : PageModel
                 Description = result.Data.Description,
                 Id = result.Data.Id,
                 Instagram = result.Data.Instagram,
-                Longitude = result.Data.Longitude?? 52.5400,
-                Latitude = result.Data.Latitude?? 29.6100,
+                Longitude = result.Data.Longitude ?? 52.5400,
+                Latitude = result.Data.Latitude ?? 29.6100,
                 LogoImage = result.Data.LogoImage != null ?
                         result.Data.LogoImage : null
             };
-            return Page();
         }
-        else
-        {
-            return RedirectToPage("/");
-        }
+        return response;
     }
 }
---
+++ SaharBeautyWeb/Pages/Shared/LandingBasePageModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SaharBeautyWeb.Models.Commons.Dtos;
+
+namespace SaharBeautyWeb.Pages.Shared;
+
+public abstract class LandingBasePageModel : PageModel
+{
+
+    protected IActionResult HandleApiResult<T>(ApiResultDto<T> result, string? successPage = null)
+    {
+        if (result == null) return RedirectToPage("/Landing/Error", new { message = "پاسخی از سرور دربافت نشد" });
+
+        if (result.IsSuccess && (result.StatusCode == 200 || result.StatusCode == 204))
+
+
+            return string.IsNullOrEmpty(successPage)
+                ? Page()
+                : RedirectToPage(successPage);
+        string message = result.Error ?? result.StatusCode switch
+        {
+            400 => "درخواست نامعتبر است.",
+            401 => "دسترسی غیرمجاز.",
+            403 => "اجازه دسترسی ندارید.",
+            404 => "موردی یافت نشد.",
+            500 => "خطای داخلی سرور.",
+            _ => "خطایی در پردازش درخواست رخ داده است."
+        };
+
+        return RedirectToPage("/Landing/Error", new { message });
+    }
+
+}
