e043abfca7f1f1e75dca72f758b9d59fc0a34da3
Reza <rdehghani.akorn@gmail.com>
2025-10-07 13:46:16 +0330

    banner landing view component error was handled

--- SaharBeautyWeb/Pages/Shared/Components/BannerLanding/BannerLandingViewComponent.cs
+++ SaharBeautyWeb/Pages/Shared/Components/BannerLanding/BannerLandingViewComponent.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SaharBeautyWeb.Models.Commons.Dtos;
+using SaharBeautyWeb.Models.Entities.Banners.Landing;
 using SaharBeautyWeb.Pages.Shared.Components.LandingBaseComponent;
 using SaharBeautyWeb.Services.Banners;
 
 namespace SaharBeautyWeb.Pages.Shared.Components.BannerLanding;
 
-public class BannerLandingViewComponent : ViewComponent
+public class BannerLandingViewComponent : LandingBaseViewComponent
 {
     private readonly IBannerService _service;
 
@@ -17,40 +18,22 @@ public class BannerLandingViewComponent : ViewComponent
     public async Task<IViewComponentResult> InvokeAsync()
     {
         var banner = await _service.Get();
-        BannerLandingModel model;
-        if (banner.IsSuccess && banner.Data != null)
+
+        var model = new ApiResultDto<BannerLandingModel>()
         {
-            model = new BannerLandingModel()
+            Data = banner.Data != null ? new BannerLandingModel()
             {
-                Title = (banner.Data.Title),
-                CreateDate = (banner.Data.CreateDate),
-                ImageName = (banner.Data.ImageName),
-                URL = (banner.Data.URL),
+                URL = banner.Data.URL,
+                ImageName = banner.Data.ImageName,
+                Title = banner.Data.Title,
+                CreateDate=banner.Data.CreateDate
+            } : null,
+            Error = banner.Error,
             IsSuccess = banner.IsSuccess,
             StatusCode = banner.StatusCode
         };
-        }
-        else
-        {
-            model = new BannerLandingModel()
-            {
-                IsSuccess = false,
-                Error = banner.Error,
-                StatusCode = banner.StatusCode,
-            };
-        }
-
-        return View(model);
+        return HandleApiResult(model);
     }
 }
 
 
-
-
-public class BannerLandingModel : ViewComponentErrorDto
-{
-    public string? Title { get; set; }
-    public string? URL { get; set; }
-    public string? ImageName { get; set; }
-    public DateTime? CreateDate { get; set; }
-}
