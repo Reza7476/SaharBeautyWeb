e27bd91f44853b4b5982617af69ef3479fffcf55
Reza7476 <rdehghani.akorn@gmail.com>
2025-10-29 21:34:28 +0330

    change day week

--- SaharBeautyWeb/Models/Entities/WeeklySchedules/Dtos/GetWeeklySchedulesDto.cs
+++ SaharBeautyWeb/Models/Entities/WeeklySchedules/Dtos/GetWeeklySchedulesDto.cs
@@ -10,11 +10,12 @@ public class GetWeeklySchedulesDto
 }
 public enum DayWeek : byte
 {
-    Saturday = 0,
-    Sunday = 1,
-    Monday = 2,
-    Tuesday = 3,
-    Wednesday = 4,
-    Thursday = 5,
-    Friday = 6,
+    None = 0,
+    Saturday = 1,
+    Sunday = 2,
+    Monday = 3,
+    Tuesday = 4,
+    Wednesday = 5,
+    Thursday = 6,
+    Friday = 7,
 }
