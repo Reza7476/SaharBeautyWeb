f841af557befb07d546226eb580100f4a8894895
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-11 12:19:06 +0330

    get all users

--- SaharBeautyWeb/Services/UserPanels/Users/IUserService.cs
+++ SaharBeautyWeb/Services/UserPanels/Users/IUserService.cs
@@ -9,5 +9,11 @@ public interface IUserService : IService
     Task<ApiResultDto<object>> EditAdminProfile(EditAdminProfileDto dto);
     Task<ApiResultDto<object>> EditClientProfile(EditClientProfileDto dto);
     Task<ApiResultDto<object>> EditProfileImage(EditMediaDto dto);
+
+    Task<ApiResultDto<GetAllDto<GetAllUsersDto>>>
+        GetAllUsers(int offset,
+        int limit, 
+        string? search);
+    
     Task<ApiResultDto<GetUserInfoDto?>> GetUserInfo();
 }
---
+++ SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/Users/users.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#user-search").on("input", function () {
+        // وقتی طول ورودی >= 3 یا خالی شد، فرم را submit کن
+        if ($(this).val().length >= 3 || $(this).val().length === 0) {
+            $("#search-form")[0].submit();
+        }
+    });
+});
