ed6b8a2a68cff3576ad9d7eb5fdb92a95436e4b0
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-13 12:44:00 +0330

    fix

--- SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/Users/users.js
+++ SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/Users/users.js
@@ -1,10 +1,13 @@
 ﻿$(document).ready(function () {
     $("#user-search").on("input", function () {
-        // وقتی طول ورودی >= 3 یا خالی شد، فرم را submit کن
-        if ($(this).val().length >= 3 || $(this).val().length === 0) {
-            $("#search-form")[0].submit();
-        }
+        
+    const value = $(this).val().trim();
+
+    if (value.length >= 11) {
+        $("#search-form")[0].submit();
+    }
     });
+
 });
 
 $(document).on("change", ".toggle-switch", function () {
