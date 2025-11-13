4d7e1bc438a8a1e44335ebaad8dd53de07efca0d
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-13 12:53:56 +0330

    hdfji

--- SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/SMS-Logs/sms-log.js
+++ SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/SMS-Logs/sms-log.js
@@ -2,9 +2,11 @@
 
 $("#searchBox").on("input", function () {
 
-    const value = $(this).val().trim();
-
-    if (value.length >= 11) {
+    const value = $(this).val().replace(/\D/g, "");
+    $(this).val(value);
+    if (value.length === 11) {
+        $("#search-form")[0].submit();
+    } else if (value.length === 0) {
         $("#search-form")[0].submit();
     }
 });
--- SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/Users/users.js
+++ SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/Users/users.js
@@ -1,14 +1,15 @@
-﻿$(document).ready(function () {
-    $("#user-search").on("input", function () {
-        
-    const value = $(this).val().trim();
+﻿
+$("#user-search").on("input", function () {
 
-    if (value.length >= 11) {
+    const value = $(this).val().replace(/\D/g, "");
+    $(this).val(value);
+    if (value.length === 11) {
         $("#search-form")[0].submit();
-    }
+    } else if (value.length === 0) {
+        $("#search-form")[0].submit();
     });
 
-});
+
 
 $(document).on("change", ".toggle-switch", function () {
     const userCard = $(this).closest(".user-card");
@@ -18,7 +19,7 @@ $(document).on("change", ".toggle-switch", function () {
 
     $.ajax({
         url: changeUserActivate,
-        type:'Post',
+        type: 'Post',
         data: {
             userId: userId,
             __RequestVerificationToken: token,
