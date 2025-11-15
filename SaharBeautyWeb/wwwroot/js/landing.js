fdb214dd8b772409b70285f9e7872e4bb91e40ef
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-15 12:37:09 +0330

    change layout

---
+++ SaharBeautyWeb/wwwroot/js/landing.js
@@ -0,0 +1,23 @@
+﻿
+
+// Simple Horizontal Scroll for Gallery & Services (Mouse Wheel)
+const horizontalScroll = (selector) => {
+    const container = document.querySelector(selector);
+    if (container) {
+        container.addEventListener('wheel', (e) => {
+            e.preventDefault();
+            container.scrollLeft += e.deltaY;
+        });
+    }
+}
+horizontalScroll('.services-wrapper');
+horizontalScroll('.gallery-slider');
+horizontalScroll('.testimonial-slider');
+
+
+const mobileButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show');
+});
