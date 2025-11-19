65d985c76a0dfa933c67c02f5fab49472614ea38
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-19 17:56:37 +0330

    banner ajax error was handled

--- SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/SiteSettings/Banners/BannerJquery.js
+++ SaharBeautyWeb/wwwroot/Layout/Admin/assets/js/pages/UserPanels/Admin/SiteSettings/Banners/BannerJquery.js
@@ -7,76 +7,41 @@
 
     $(document).on("click","#createBanner" ,function (e) {
         e.preventDefault();
-        var btnSend = $(this);
         var imageInput = $(".get-banner-image")[0];
         var viewImg = $(".preview-new-banner-image")[0];
         setUpImagePreview(imageInput, viewImg);
     
         var form = $("#add-banner-form")[0]
         let formData = new FormData(form);
-
-        btnSend.prop("disabled", true);
-        $.ajax({
+        ajaxWithButtonLoading({
+            button: "#createBanner",
             url: createBanner,
             type: 'POST',
             data: formData,
             contentType: false,
             processData: false,
-            beforeSend: function () {
-                btnSend.prop("disabled", true);
-            },
             success: function (res) {
-                if (res.success) {
-                    $("#add-banner-form")[0].reset();
-                    location.reload();
-                } else {
-                    handleApiError(res.error);
-                    btnSend.prop("disabled", false);
-                    return
-                }
-            },
-            error: function (xhr) {
-
-                handleApiError(xhr);
-                btnSend.prop("disabled", false);
-                return;
+                location.reload();
             }
-        });
+        })
     });
 
     $(".edit-banner-btn").on("click", function () {
         var bannerId = $(this).data("id");
         var btnSend = $(this);
-        $(this).prop("disabled", true);
-        $.ajax({
+
+        ajaxWithButtonLoading({
+            button: this,// $(this),//".edit-banner-btn",
             url: editBannerPartial,
             type: 'GET',
             data: { id: bannerId },
             success: function (res, status, xhr) {
-                const contentType = xhr.getResponseHeader("content-type") || "";
-                if (contentType.includes("application/json")) {
-                    if (!res.success) {
-                        handleApiError(res.error);
-                        btnSend.prop("disabled", false);
-                        return;
-                    }
-                }
+                // در این نقطه فقط وقتی HTML معتبر بود وارد می‌شود
                 $("#staticBackdrop .modal-body").html(res);
-                var modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
-                modal.show();
+                new bootstrap.Modal(document.getElementById('staticBackdrop')).show();
             },
-            error: function () {
-                showPopup("خطا در بارگذاری فرم ویرایش!");
-                btnSend.prop("disabled", false);
-            },
-            complete: function () {
-
-                btnSend.prop("disabled", false);
-            }
         });
     })
-
-  
 
     $(document).on("click", "#saveBannerBtn", function () {
 
@@ -87,35 +52,19 @@
 
         var form = $("#editBannerForm")[0];
         var formData = new FormData(form);
-
-        $.ajax({
+        ajaxWithButtonLoading({
+            button: "#saveBannerBtn",
             url: editBanner,
             type: 'Post',
             data: formData,
             contentType: false,
             processData: false,
-            beforeSend: function () {
-                btnSend.prop("disabled", true);
-            },
-            success: function (res) {
-                if (res.success) {
-                    var modalEl = document.getElementById('staticBackdrop');
-                    var modal = bootstrap.Modal.getInstance(modalEl);
-                    modal.hide();
-                    $("#editBannerForm")[0].reset();
-                    location.reload();
-                } else {
-                    handleApiError(res.error);
-                    btnSend.prop("disabled", false);
-                }
-            },
-            error: function () {
-                showPopup("خطایی رخ داده");
-                var modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+            success: function (res)
+            {
+                var modalEl = document.getElementById('staticBackdrop');
+                var modal = bootstrap.Modal.getInstance(modalEl);
                 modal.hide();
-            },
-            complete: function () {
-                 btnSend.prop("disabled", false);
+                location.reload();
             }
         });
     });
